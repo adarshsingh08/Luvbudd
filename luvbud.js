9b9c17de4a8147ca6a3b3f9da3ed6d808ea94a55
Adarsh Singh <69454312+adarshsingh08@users.noreply.github.com>
2023-07-15 12:58:23 +0530

    Update luvbud.js
    love update

--- luvbud.js
+++ luvbud.js
@@ -1,5 +1,6 @@
 function myfunction(){
     var n=Math.random();
     var res=Math.round(n*100);
-    document.getElementById("result").innerHTML="Your love score is "+res+" %.";
+    //document.getElementById("result").innerHTML="Your love score is "+res+" %.";
+    document.getElementById("result").innerHTML="Adarsh Loves you very much no need to check.%.";
 }
